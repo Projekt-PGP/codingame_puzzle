1e7d01f386ed670c3a4396db8ffca6606a221d02
Ganobis <kasiaodi@gmail.com>
2021-03-11 22:11:06 +0100

    First push

--- game-skeleton-master/src/main/java/com/codingame/game/Referee.java
+++ game-skeleton-master/src/main/java/com/codingame/game/Referee.java
@@ -3,14 +3,12 @@ import java.util.List;
 
 import com.codingame.gameengine.core.AbstractPlayer.TimeoutException;
 import com.codingame.gameengine.core.AbstractReferee;
-import com.codingame.gameengine.core.MultiplayerGameManager;
+import com.codingame.gameengine.core.SoloGameManager;
 import com.codingame.gameengine.module.entities.GraphicEntityModule;
 import com.google.inject.Inject;
 
 public class Referee extends AbstractReferee {
-    // Uncomment the line below and comment the line under it to create a Solo Game
-    // @Inject private SoloGameManager<Player> gameManager;
-    @Inject private MultiplayerGameManager<Player> gameManager;
+    @Inject private SoloGameManager<Player> gameManager;
     @Inject private GraphicEntityModule graphicEntityModule;
 
     @Override
@@ -20,18 +18,6 @@ public class Referee extends AbstractReferee {
 
     @Override
     public void gameTurn(int turn) {
-        for (Player player : gameManager.getActivePlayers()) {
-            player.sendInputLine("input");
-            player.execute();
-        }
 
-        for (Player player : gameManager.getActivePlayers()) {
-            try {
-                List<String> outputs = player.getOutputs();
-                // Check validity of the player output and compute the new game state
-            } catch (TimeoutException e) {
-                player.deactivate(String.format("$%d timeout!", player.getIndex()));
-            }
-        }        
     }
 }
--- game-skeleton-master/src/test/java/SkeletonMain.java
+++ game-skeleton-master/src/test/java/SkeletonMain.java
@@ -1,11 +1,9 @@
-import com.codingame.gameengine.runner.MultiplayerGameRunner;
+import com.codingame.gameengine.runner.SoloGameRunner;
 
 public class SkeletonMain {
     public static void main(String[] args) {
 
-        // Uncomment this section and comment the other one to create a Solo Game
-        /* Solo Game */
-        // SoloGameRunner gameRunner = new SoloGameRunner();
+        SoloGameRunner gameRunner = new SoloGameRunner();
 
         // Sets the player
         // gameRunner.setAgent(Player1.class);
@@ -13,16 +11,8 @@ public class SkeletonMain {
         // Sets a test case
         // gameRunner.setTestCase("test1.json");
 
-        /* Multiplayer Game */
-        MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
-
         // Adds as many player as you need to test your game
-        gameRunner.addAgent(Agent1.class);
-        gameRunner.addAgent(Agent2.class);
-
-        // Another way to add a player
-        // gameRunner.addAgent("python3 /home/user/player.py");
-        
+        gameRunner.setAgent(Agent.class);
 
         gameRunner.start();
     }
