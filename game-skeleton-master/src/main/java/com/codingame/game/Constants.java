d6602e803b0e9a2a3f3ffa9fe803eb682cc0bb0f
Pandem95 <33704582+Pandem95@users.noreply.github.com>
2021-03-12 11:50:57 +0100

    Referee update v0.1

---
+++ game-skeleton-master/src/main/java/com/codingame/game/Constants.java
@@ -0,0 +1,16 @@
+package com.codingame.game;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.codingame.gameengine.module.entities.World;
+
+public class Constants {
+    public static final int VIEWER_WIDTH = World.DEFAULT_WIDTH;
+    public static final int VIEWER_HEIGHT = World.DEFAULT_HEIGHT;
+    
+    public static final String PLAYER_SPRITE = "player.png";
+    public static final String ENEMY_SPRITE = "enemy.png";
+    
+    public static final String MOVE_ACTION = "MOVE";
+}
