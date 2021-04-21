c6f161d18534d153030c3060633a53b2f539f96c
Pandem95 <33704582+Pandem95@users.noreply.github.com>
2021-04-21 22:09:56 +0200

    hp and stamina and test2

--- game-skeleton-master/src/main/java/com/codingame/game/Player.java
+++ game-skeleton-master/src/main/java/com/codingame/game/Player.java
@@ -1,9 +1,6 @@
 package com.codingame.game;
 import com.codingame.gameengine.core.AbstractSoloPlayer;
 
-import java.sql.Time;
-
-
 public class Player extends AbstractSoloPlayer {
     @Override
     public int getExpectedOutputLines() {
