ca334158e4a5b0fbeb0f635069322d9c5570bfb3
Pandem95 <33704582+Pandem95@users.noreply.github.com>
2021-04-23 00:06:45 +0200

    test3 ready

--- game-skeleton-master/src/test/java/SkeletonMain.java
+++ game-skeleton-master/src/test/java/SkeletonMain.java
@@ -9,6 +9,7 @@ public class SkeletonMain {
         // Sets a test case
         gameRunner.setTestCase("test2.json");
         gameRunner.setTestCase("test1.json");
+        gameRunner.setTestCase("test3.json");
 
 
         // Adds as many player as you need to test your game
