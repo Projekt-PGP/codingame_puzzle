d1ea451a652bb923a41e16a988d095e346f93166
Pandem95 <33704582+Pandem95@users.noreply.github.com>
2021-04-23 00:25:29 +0200

    test4

--- game-skeleton-master/src/test/java/Agent.java
+++ game-skeleton-master/src/test/java/Agent.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class Agent {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int[] tab={4,5,3,6};
         int i=0;
         while (true) {
             //String input = scanner.nextLine();
-            System.out.println(i);
+            System.out.println(tab[i]);
             i++;
         }
     }
--- game-skeleton-master/src/test/java/SkeletonMain.java
+++ game-skeleton-master/src/test/java/SkeletonMain.java
@@ -10,6 +10,7 @@ public class SkeletonMain {
         gameRunner.setTestCase("test2.json");
         gameRunner.setTestCase("test1.json");
         gameRunner.setTestCase("test3.json");
+        //gameRunner.setTestCase("test4.json");
 
 
         // Adds as many player as you need to test your game
